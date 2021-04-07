bc36708596964e6e232535850491e5c541f81514
aabinlo <zhangzhixieji@126.com>
2021-04-07 16:32:05 +0800

    添加处理错误的组件。

---
+++ views/error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+var Error = React.createClass({
+    render: function() {
+        return (
+            <h1>ERROR, oops!</h1>
+        );
+    }
+});
+
+module .exports = Error;
