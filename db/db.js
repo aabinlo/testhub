f0b04aba65955b39785f67eda972634e0b2c174f
aabinlo <zhangzhixieji@126.com>
2021-04-13 15:15:52 +0800

    完善数据库和devicemonitor通信部分代码

--- db/db.js
+++ db/db.js
@@ -6,30 +6,29 @@ var dbCfg = require('./db-cfg');
 
 var pool = mysql.createPool(dbCfg);
 
-function sqlQuery(sql, args, handler){
-    pool.getConnection(function(err, conn){
-        if(err){
+function sqlQuery(sql, args, handler) {
+    pool.getConnection(function (err, conn) {
+        if (err) {
             console.log('connect sql server failed ' + err.message);
             handler(err);
-        }else{
-            if(args){
-                var query = conn.query(sql, args, function(qerr, results){
-                    if(qerr){
+        } else {
+            if (args) {
+                var query = conn.query(sql, args, function (qerr, results, fields) {
+                    if (qerr) {
                         console.log('query sql error ' + sql.toString() + ' ' + qerr.message);
                         handler(qerr);
+                    }else {
+                        handler(null, results, fields);
                     }
-
-                    handler(null, results);
                 });
-            }
-            else {
-                var query = conn.query(sql, function(qerr, results){
-                    if(qerr){
+            } else {
+                var query = conn.query(sql, function (qerr, results, fields) {
+                    if (qerr) {
                         console.log('query sql error ' + sql.toString() + ' ' + qerr.message);
                         handler(qerr);
+                    }else {
+                        handler(null, results, fields);
                     }
-
-                    handler(null, results);
                 });
             }
             conn.release();
--- routes/device.js
+++ routes/device.js
@@ -2,26 +2,25 @@ var express = require('express');
 var mysql = require('../db/db-device');
 var router = express.Router();
 
-var getResponse = function(code){
+var getResponse = function (code) {
     var resInfo = {
-        'code':0,
-        'message':'',
-        'request_id':''
+        'code': 0,
+        'message': '',
+        'request_id': ''
     };
 
     resInfo.code = code;
     var date = new Date();
     resInfo.request_id = (date.getTime()).toString();
-    switch(code)
-    {
+    switch (code) {
         case 0:
             resInfo.message = 'OK';
             break;
         case -1:
-            resInfo.message = 'connect db failed';
+            resInfo.message = 'query db failed';
             break;
         case -2:
-            resInfo.message = 'query db failed';
+            resInfo.message = 'unexpect error';
             break;
         default:
             resInfo.message = 'unknown error';
@@ -30,39 +29,34 @@ var getResponse = function(code){
     return resInfo;
 };
 
-var cb = function(err, results){
-    if(!err){
-        res.json(getResponse(retCode));
-    }else {
-        console.log('sql query err : ' + err.message);
+router.post('/init', function (req, res) {
+    try {
+        var deviceInfo = req.body;
+        deviceInfo['logo'] = 'test.logo';
+        deviceInfo['sockio_port'] = 0;
+
+        mysql.selectDevice(deviceInfo, function (err, results) {
+            if (err) {
+                res.json(getResponse(-1));
+            } else {
+                if (typeof results != 'undefined' && results.length > 0) {
+                    console.log('update device ' + deviceInfo.brand_name);
+                    mysql.updateDevice(deviceInfo, function (err, results) {
+                        var code = err ? -1 : 0;
+                        res.json(getResponse(code));
+                    });
+                } else {
+                    console.log('insert new device ' + deviceInfo.brand_name);
+                    mysql.insertDevice(deviceInfo, function (err, results) {
+                        var code = err ? -1 : 0;
+                        res.json(getResponse(code));
+                    });
+                }
+            }
+        });
+    } catch (err) {
+        res.json(getResponse(-2));
     }
-};
-
-router.post('/init', function(req, res){
-    var deviceInfo = req.body;
-    deviceInfo['logo'] = 'test.logo';
-    deviceInfo['sockio_port'] = 0;
-
-    mysql.selectDevice(deviceInfo, function(err, retCode, results){
-        if(!err){
-            if(results == 0){
-                console.log('insert new device ' + deviceInfo.brand_name);
-                mysql.insertDevice(deviceInfo, function(err, results){
-                    if(!err){
-                        res.json(getResponse(retCode));
-                    }else {
-                        console.log('sql query err : ' + err.message);
-                    }
-                });
-            }
-            else {
-                console.log('update device ' + deviceInfo.brand_name);
-                mysql.updateDevice(deviceInfo, function(err, results){
-                    res.json(getResponse(retCode));
-                });
-            }
-        }
-    });
 });
 
 module.exports = router;
