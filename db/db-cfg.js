451eb9e4b5e422dd0521f4d89dab8e6bfc40ef8f
aabinlo <zhangzhixieji@126.com>
2021-04-18 16:57:39 +0800

    根据运行环境，决定选择哪个数据库。

--- db/db-cfg.js
+++ db/db-cfg.js
@@ -1,7 +1,13 @@
-module.exports = {
+var dbCfg = {
     host: 'th.lo',
     user: 'th',
     password: 'testhub',
     database: 'testhub_dev',
     port: 3306
 };
+
+if (process.env.NODE_ENV === 'production') {
+    dbCfg.database = 'testhub';
+}
+
+module.exports = dbCfg;
